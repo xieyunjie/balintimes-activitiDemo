a446f9f72eb3d825809edbbbef3472184f72b9f2
xieyunjie <yunjie.xie@hotmail.com>
2015-10-12 11:51:55 +0800

    加入中止、激活流程定义 中止、激活流程

--- src/main/java/spring/LoopWFCtrl.java
+++ src/main/java/spring/LoopWFCtrl.java
@@ -7,9 +7,11 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.activiti.engine.ActivitiException;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
@@ -34,7 +36,7 @@ public class LoopWFCtrl {
 	@Test
 	public void deployBPNM() {
 
-		Deployment deployment = this.repositoryService.createDeployment().name("LoopCardinality����").addClasspathResource("spring/LoopWF.bpmn")
+		Deployment deployment = this.repositoryService.createDeployment().name("LoopWF流程").addClasspathResource("spring/LoopWF.bpmn")
 				.addClasspathResource("spring/LoopWF.png").deploy();
 
 		System.out.println(deployment);
@@ -44,35 +46,41 @@ public class LoopWFCtrl {
 		System.out.println("name:" + processDefinition.getName());
 	}
 
+	private String bizKey = "crm-ba1a1840-6d6c-11e5-b1fb-c86000a05d5f";
 	@Test
 	public void submitBPNM() {
 
-		String creator = "小A";
-		String bizKey = "crm-ba1a1840-6d6c-11e5-b1fb-c86000a05d5f";
+		try {
 
-		Map<String, Object> variablesMap = new HashMap<String, Object>(2);
-		variablesMap.put("creator", creator);
-		List<String> audits = new ArrayList<String>(3);
-		audits.add("小E");
-		audits.add("小D"); 
-		variablesMap.put("audits", audits); 
-		ProcessInstance pi = runtimeService.startProcessInstanceByKey("LoopWFProcess", bizKey, variablesMap);
-		System.out.println("pid:" + pi.getId() + " activitiID:" + pi.getActivityId() + " pdID:" + pi.getProcessDefinitionId());
+			String creator = "小A"; 
 
+			Map<String, Object> variablesMap = new HashMap<String, Object>(2);
+			variablesMap.put("creator", creator);
+			List<String> audits = new ArrayList<String>(3);
+			audits.add("小E");
+			audits.add("小D");
+			variablesMap.put("audits", audits);
+			ProcessInstance pi = runtimeService.startProcessInstanceByKey("LoopWFProcess", bizKey, variablesMap);
+			System.out.println("pid:" + pi.getId() + " activitiID:" + pi.getActivityId() + " pdID:" + pi.getProcessDefinitionId());
 
-		// this.taskService.createTaskQuery().processInstanceBusinessKey(bizKey).singleResult();
-		Task task = this.taskService.createTaskQuery().taskAssignee(creator).orderByTaskCreateTime().desc().list().get(0);
-		this.taskService.complete(task.getId());
-		System.out.println("完成提交");
+			// this.taskService.createTaskQuery().processInstanceBusinessKey(bizKey).singleResult();
+			Task task = this.taskService.createTaskQuery().taskAssignee(creator).orderByTaskCreateTime().desc().list().get(0);
+			this.taskService.complete(task.getId());
+			System.out.println("完成提交");
+		} catch (ActivitiException exception) {
+			
+			System.out.println(exception);
+			// TODO: handle exception 
+		}
 	}
-	
+
 	@Test
-	public void queryTask() {
+	public void queryAndCompleteTask() {
 		String creator = "小e";
-		
+
 		Task task = this.taskService.createTaskQuery().taskAssignee(creator).orderByTaskCreateTime().desc().list().get(0);
 		this.taskService.complete(task.getId());
-		
+
 		System.out.println(task.getId());
 	}
 
@@ -80,29 +88,52 @@ public class LoopWFCtrl {
 	public void getTask() {
 		String assignee = "小E";
 
-		List<Task> tasks = taskService.createTaskQuery().taskAssignee(assignee).orderByTaskCreateTime().asc().list();
+		List<Task> tasks = taskService.createTaskQuery().active().taskAssignee(assignee).orderByTaskCreateTime().asc().list();
 
 		for (Task task : tasks) {
 			System.out.println(" id:" + task.getId());
 			System.out.println(" name:" + task.getName());
 			System.out.println(" createtime:" + task.getCreateTime());
-			System.out.println(" assignee:" + task.getAssignee()); 
+			System.out.println(" assignee:" + task.getAssignee());
 		}
 	}
 
 	@Test
 	public void completeTask() {
 		String taskId = "2518";
-		this.taskService.complete(taskId); 
+		this.taskService.complete(taskId);
 	}
 
-	
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	@Test
 	public void TestService() {
 		System.out.println(employeeService.getMsg());
 	}
+
+	/**
+	 * 中止/激活流程定义 中止只能阻止流程发起，已发起的不影响。
+	 * */
+	@Test
+	public void suspendProcessDef() { 
+		//this.repositoryService.suspendProcessDefinitionByKey("LoopWFProcess");
+		this.repositoryService.activateProcessDefinitionByKey("LoopWFProcess");
+		System.out.println("suspend");
+
+	}
 	
+	/**
+	 * 暂停流程，暂停后再查询个人任务时，需要加入 .active()，如：
+	 * taskService.createTaskQuery().active().taskAssignee(assignee).orderByTaskCreateTime().asc().list()
+	 * */
+	@Test
+	public void suspendProcessInst(){ 
+		ProcessInstance pi = this.runtimeService.createProcessInstanceQuery().processInstanceBusinessKey(bizKey).singleResult();
+		this.runtimeService.suspendProcessInstanceById(pi.getId());
+		
+		ExecutionEntity executionEntity = (ExecutionEntity) this.runtimeService.createExecutionQuery().processInstanceBusinessKey(bizKey).singleResult();
+		
+		System.out.println("suspend??  " + executionEntity.isSuspended()); 
+	}
 }
